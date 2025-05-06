ec07a4bf190789e1da97f95f977c36ab0303e475
krillijuni <kristleifur@juni.is>
2025-05-06 15:24:00 +0000

    Add main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
-  // Enable CORS
+
   app.enableCors({
     origin: true, // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
